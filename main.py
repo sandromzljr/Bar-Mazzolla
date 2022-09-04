49d09fe599000917204a7e84dbe54856de9eed10
sandromzljr <jmazzolla94@gmail.com>
2022-09-04 17:18:13 -0300

    Main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import FormCriarConta, FormLogin
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'bar_mazzolla'
 
 
 @app.route('/')
@@ -8,9 +12,27 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+
 @app.route('/localizacao')
 def localizacao():
     return render_template('localizacao.html')
+
+
+@app.route('/lista-usuarios')
+def usuarios():
+    userList = ['Sandro', 'Marina', 'Augusto', 'Jean']
+    return render_template('user.html', userList=userList)
+
+
+@app.route('/login')
+def login():
+    formLogin = FormLogin()
+    formCriarConta = FormCriarConta()
+    return render_template('login.html', formLogin=formLogin, formCriarConta=formCriarConta)
 
 
 if __name__ == '__main__':
