a740f8e1528c5fe077e3c041b1f9c049420bbe52
sandromzljr <jmazzolla94@gmail.com>
2022-09-18 13:20:35 -0300

    Models

---
+++ models.py
@@ -0,0 +1,19 @@
+from main import database
+from datetime import datetime
+
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    senha = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    foto_perfil = database.Column(database.String, default='default.jpg')
+    posts = database.relationship('Post', backref='autor', lazy=True)
+
+
+class Post(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
+    titulo = database.Column(database.String, nullable=False)
+    corpo = database.Column(database.Text, nullable=False)
+    data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
