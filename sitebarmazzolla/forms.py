55fc3c1132fc7c7774d42db2288aa73b9f01ae94
sandromzljr <jmazzolla94@gmail.com>
2022-11-02 17:34:51 -0300

    feature: add code for edit profile, add code for edit photo profile

--- sitebarmazzolla/forms.py
+++ sitebarmazzolla/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from sitebarmazzolla.models import Usuario
+from flask_login import current_user
 
 
 class FormCriarConta(FlaskForm):
@@ -28,4 +30,11 @@ class FormLogin(FlaskForm):
 class FormEditarPerfil(FlaskForm):
     username = StringField('Nome de Usuário', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de Perfil:', validators=[FileAllowed(['jpg', 'png'])])
     btn_submit_edit_perfil = SubmitField('Confirmar Edição')
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('E-mail já cadastrado. Utilize outro e-mail para continuar.')
